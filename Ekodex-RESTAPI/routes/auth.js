b5fac4d0b7ccc168078c748a4e537cdd1784331a
Victor Erickson <50758632+vericks101@users.noreply.github.com>
2020-05-08 13:56:44 -0700

    Added comments to register and verification for clarification.

--- Ekodex-RESTAPI/routes/auth.js
+++ Ekodex-RESTAPI/routes/auth.js
@@ -31,6 +31,7 @@ router.post('/register', async (req, res) => {
         password: hashedPassword
     });
 
+    // Save the new user.
     try {
         await user.save();
         res.send({ user: user._id });
